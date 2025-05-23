6a2cbf7c6669dcbe60c9e2c7d4ca79e186cf7718
Dominik Krenn <dominik.krenn@1tool.com>
2025-05-23 13:42:38 +0200

    Add a_nop action class with execute method returning true

---
+++ Interface/action/a_nop.java
@@ -0,0 +1,10 @@
+package Interface.action;
+
+import Interface.Action;
+import Interface.Interface;
+
+public class a_nop extends Action {
+    public Boolean execute(Interface base) {
+        return true;
+    };
+}
